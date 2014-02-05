6447f35a9b00964bf2529c26b3fc9c7d0134fc89
Kyle Kellogg <kyle@kylekellogg.com>
2014-02-05 03:22:32 -0500

    Updating main.lua to have options

--- main.lua
+++ main.lua
@@ -1,16 +1,111 @@
+#!/usr/bin/env lua
+
+local flags = {
+	wasd = false,
+	arrows = false,
+	keys = false,
+
+	title = '',
+	width = 800,
+	height = 600
+}
+
+local a = nil
+local b = nil
+local s = nil
+
+for _,v in ipairs( arg ) do
+	s = string.find( v, '=' )
+	if s then
+		a = string.sub( v, 1, s-1 )
+		b = string.sub( v, s+1 )
+
+		if flags[a] ~= nil then
+			flags[a] = b
+		end
+	end
+end
+
 os.execute( 'touch main.lua' )
 os.execute( 'touch conf.lua' )
 
 local mainFile = io.open( 'main.lua', 'w+' )
 assert( io.type( mainFile ) == 'file', 'File main.lua was not opened successfully' )
 
-mainFile:write( [[
+local hasKeys = flags.keys or flags.wasd or flags.arrows
+local keyLvl = flags.keys and 1 or flags.wasd and flags.arrows and 1 or flags.wasd and 2 or flags.arrows and 3 or 0
+local keyVars = not hasKeys and '' or [[
+local keyDown = {
+	left = false,
+	right = false,
+	down = false,
+	up = false
+}
+]]
+local keyIs = 'key == '
+local keyStr = {
+	w = keyIs.."'w'",
+	up = keyIs.."'up'",
+	s = keyIs.."'s'",
+	down = keyIs.."'down'",
+	a = keyIs.."'a'",
+	left = keyIs.."'left'",
+	d = keyIs.."'d'",
+	right = keyIs.."'right'"
+}
+local upStr = keyLvl == 1 and keyStr.w..' or '..keyStr.up or keyLvl == 2 and keyStr.w or keyLvl == 3 and keyStr.up or ''
+local downStr = keyLvl == 1 and keyStr.s..' or '..keyStr.down or keyLvl == 2 and keyStr.s or keyLvl == 3 and keyStr.down or ''
+local leftStr = keyLvl == 1 and keyStr.a..' or '..keyStr.left or keyLvl == 2 and keyStr.a or keyLvl == 3 and keyStr.left or ''
+local rightStr = keyLvl == 1 and keyStr.d..' or '..keyStr.right or keyLvl == 2 and keyStr.d or keyLvl == 3 and keyStr.right or ''
+local keypressed = not hasKeys and '' or [[
+if ]]..upStr..[[ then
+		keyDown.up = true
+	elseif ]]..leftStr..[[ then
+		keyDown.left = true
+	elseif ]]..downStr..[[ then
+		keyDown.down = true
+	elseif ]]..rightStr..[[ then
+		keyDown.right = true
+	end
+]]
+local keyreleased = not hasKeys and '' or [[
+if ]]..upStr..[[ then
+		keyDown.up = false
+	elseif ]]..leftStr..[[ then
+		keyDown.left = false
+	elseif ]]..downStr..[[ then
+		keyDown.down = false
+	elseif ]]..rightStr..[[ then
+		keyDown.right = false
+	end
+]]
+local keyupdate = not hasKeys and '' or [[
+if keyDown.up then
+		-- Up movement here
+	end
+
+	if keyDown.down then
+		-- Down movement here
+	end
+
+	if keyDown.left then
+		-- Left movement here
+	end
+
+	if keyDown.right then
+		-- Right movement here
+	end
+]]
+
+mainFile:write( keyVars..[[
+
 --	Main functions
 
-function love.load()
+function love.load( arg )
 end
 
-function love.update()
+function love.update( dt )
+	]]..keyupdate..[[
 end
 
 function love.draw()
@@ -18,37 +113,39 @@ end
 
 --	Key and text input handling
 
-function love.keypressed()
+function love.keypressed( key, isrepeat )
+	]]..keypressed..[[
 end
 
-function love.keyreleased()
+function love.keyreleased( key )
+	]]..keyreleased..[[
 end
 
-function love.textinput()
+function love.textinput( text )
 end
 
 --	Mouse handling
 
-function love.mousepressed()
+function love.mousepressed( x, y, button )
 end
 
-function love.mousereleased()
+function love.mousereleased( x, y, button )
 end
 
 --	Focus handling
 
-function love.focus()
+function love.focus( f )
 end
 
-function love.mousefocus()
+function love.mousefocus( f )
 end
 
-function love.visible()
+function love.visible( v )
 end
 
 --	Resize handling
 
--- function love.resize()
+-- function love.resize( w, h )
 -- end
 
 --	Main loop & ending func
@@ -61,10 +158,10 @@ end
 
 --	Error handling
 
--- function love.errhand()
+-- function love.errhand( msg )
 -- end
 
--- function love.threaderror()
+-- function love.threaderror( thread, errorstr )
 -- end
 
 function beginContact( a, b, coll )
@@ -96,14 +193,14 @@ function love.conf(t)
 	t.version = '0.9.0'					-- The LÖVE version this game was made for (string)
 	t.console = false					-- Attach a console (boolean, Windows only)
 
-	t.window.title = ''					-- The window title (string)
+	t.window.title = ]]..flags.title..[[					-- The window title (string)
 	t.window.icon = nil					-- Filepath to an image to use as the windows icon (string)
-	t.window.width = 800				-- The window width (number)
-	t.window.height = 600				-- The window height (number)
+	t.window.width = ]]..flags.width..[[				-- The window width (number)
+	t.window.height = ]]..flags.height..[[				-- The window height (number)
 	t.window.borderless = false			-- Remove all border visuals from the window (boolean)
 	t.window.resizable = false			-- Let the window be user-resizable (boolean)
-	t.window.minwidth = 800				-- Minimum window width if the window is resizable (number)
-	t.window.minheight = 600			-- Minimum window height if the window is resizable (number)
+	t.window.minwidth = ]]..flags.width..[[				-- Minimum window width if the window is resizable (number)
+	t.window.minheight = ]]..flags.height..[[			-- Minimum window height if the window is resizable (number)
 	t.window.fullscreen = false			-- Enable fullscreen (boolean)
 	t.window.fullscreentype = 'normal'	-- Standard fullscreen or desktop fullscreen mode (string)
 	t.window.vsync = true				-- Enable vertical sync (boolean)
